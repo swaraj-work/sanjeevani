c8a518f15a9829a22f672b9e8da1e11ccbdc5ade
Swadesh-c0de <patelswadesh7@gmail.com>
2025-07-04 00:18:47 +0530

    changes

--- next.config.js
+++ next.config.js
@@ -1,3 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    images: {
+        unoptimized: true,
+    }
+};
+
 module.exports = {
     output: 'export',
 }
+
+export default nextConfig;
